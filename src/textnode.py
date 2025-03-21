2c0fc5d8c6d18155328b77ac8273750b1e92b617
Artur <artur.gawrych@gmail.com>
2025-03-21 08:01:14 +0100

    fixed __eq__, and added basic unit test

--- src/textnode.py
+++ src/textnode.py
@@ -14,8 +14,12 @@ class TextNode():
         self.text_type = TextType(text_type)
         self.url = url
 
-    def __eg__(self, other):
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+    def __eq__(self, other):
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type 
+            and self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
