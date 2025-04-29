5e4d56a300018844267b835f33ef03ac0bdc7451
Artur <artur.gawrych@gmail.com>
2025-04-29 21:41:54 +0200

    added unittest for block_markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -3,6 +3,5 @@ def markdown_to_blocks(markdown):
     blocks = []
     splitted = markdown.split("\n\n")
     for item in splitted:
-        item.strip()
-        blocks.append(item)
+        blocks.append(item.strip())
     return blocks
